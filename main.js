10c572e6086d64ea974747bfb26079fa5fbb9c1f
fancisco Jimenez <devfran08@gmail.com>
2022-12-02 22:12:03 -0300

    add : explicacion inicial de typescript

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("");
+});
+app.listen(9000, () => console.log("listening on"));
